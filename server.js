6d4f72378e0f49bdd65bf996e198528f84a11064
Phan Phuoc Hiep <22719711.hiep@student.iuh.edu.vn>
2026-06-03 03:01:35 +0700

    update

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const app = express();
 
 const TARGET = "https://orion-chat.duckdns.org";
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(
   "/",
   createProxyMiddleware({
